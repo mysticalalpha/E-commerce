780839b183325710369ae68cdfc3725dd21f2de6
Prathamesh Wakekar <128061039+mysticalalpha@users.noreply.github.com>
2025-11-08 00:14:18 +0530

    Update vite.config.ts

--- E-commerce Website UI Design/vite.config.ts
+++ E-commerce Website UI Design/vite.config.ts
@@ -5,7 +5,6 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || "/E-commerce",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
